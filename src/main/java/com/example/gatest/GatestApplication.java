52180294648c0d86679f60ec789100c3113da86e
Oh-Myeongjae <omjcws@gmail.com>
2023-02-22 17:06:46 +0900

    초기세팅

---
+++ src/main/java/com/example/gatest/GatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatestApplication.class, args);
+    }
+
+}
