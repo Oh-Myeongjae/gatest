a7a3a2f20aa6227f8bb4d72f518f9e0631ee8579
Oh-Myeongjae <omjcws@gmail.com>
2023-02-23 01:12:32 +0900

    git 액션적용

--- src/main/java/com/example/gatest/controller/main.java
+++ src/main/java/com/example/gatest/controller/main.java
@@ -1,13 +1,19 @@
 package com.example.gatest.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class main {
 
+    @Value("${enString}")
+    private String word;
+
     @GetMapping("/")
     public String print(){
-        return "git 액션적용";
+        System.out.println("word = " + word);
+        return "git 액션적용 "+word;
     }
 }
